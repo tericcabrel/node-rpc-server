8bde4c539bc96f32c3fb6e42d4faf0d9d0ad0c82
Eric Cabrel <tericcabrel@yahoo.com>
2019-12-04 11:55:30 +0100

    Create a client to consume the service

---
+++ app/client.ts
@@ -0,0 +1,26 @@
+const rpc: any = require('node-json-rpc');
+
+const options: any = {
+	port: 7016,
+	host: '127.0.0.1',
+	path: '/',
+	strict: true,
+};
+
+let serviceName: string = '';
+
+if (process.argv.length >= 3) {
+	serviceName = process.argv[2];
+}
+console.log(serviceName);
+
+// Create a client object with options
+const client: any = new rpc.Client(options);
+
+client.call({ method: 'domain_name', params: [serviceName] }, (err: any, res: any) => {
+	if (err) {
+		console.error('Error => ', err);
+	} else {
+		console.log('Result => ', res);
+	}
+});
--- app/index.ts
+++ app/index.ts
@@ -1,43 +1,38 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-const rpc = require('node-json-rpc');
+const rpc: any = require('node-json-rpc');
 
-const options = {
-  // int port of rpc server, default 5080 for http or 5433 for https
-  port: 7016,
-  // string domain name or ip of rpc server, default '127.0.0.1'
-  host: '127.0.0.1',
-  // string with default path, default '/'
-  path: '/',
-  // boolean false to turn rpc checks off, default true
-  strict: true,
+const options: any = {
+	port: 7016,
+	host: '127.0.0.1',
+	path: '/',
+	strict: false,
 };
 
 // Create a server object with options
-const server = new rpc.Server(options);
+const server: any = new rpc.Server(options);
 
-// Add your methods
+// Add a method to get a domain name
 server.addMethod('domain_name', (params: any[], callback: Function) => {
-  const servicePath: string = path.join(`${__dirname}`, '../public/service.json');
+	const servicePath: string = path.join(`${__dirname}`, '../public/service.json');
 
-  console.log('service => ', servicePath);
-  const content: string = fs.readFileSync(servicePath, { encoding: 'utf8' });
+	const content: string = fs.readFileSync(servicePath, { encoding: 'utf8' });
 
-  try {
-    const services: { [key: string]: string } = JSON.parse(content);
+	try {
+		const services: { [key: string]: string } = JSON.parse(content);
 
-    callback(null, services[params[0]] ? services[params[0]] : null);
-  } catch (e) {
-    console.error('JSON Parse error: ', e);
+		callback(null, services[params[0]] ? services[params[0]] : null);
+	} catch (e) {
+		console.error('JSON Parse error: ', e);
 
-    callback('Unexpected error!', null);
-  }
+		callback('Unexpected error!', null);
+	}
 });
 
 // Start the server
 server.start((error: any) => {
-  if (error) throw error;
+	if (error) throw error;
 
-  console.log('RPC Server is running...');
+	console.log('RPC Server is running...');
 });
